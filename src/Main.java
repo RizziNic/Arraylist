a7e71e8a8888e847e0ecad8edb8079e57d5e0906
RizziNic <rizzijunior.nico@gmail.com>
2023-05-04 13:33:54 +0200

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String s;
+        ArrayList<String> parole = new ArrayList<>();
+
+        try{
+            //BufferedReader i = new BufferedReader(new FileReader("Lettore.txt"));
+            //BufferedWriter o = new BufferedWriter(new FileWriter("Scrittore.txt"));
+
+            //FileReader i = new FileReader("Listaparole.txt");
+            //FileWriter o = new FileWriter("scrivi.txt");
+
+            FileReader f =new FileReader("Listaparole.txt");
+            BufferedReader b = new BufferedReader(f);
+
+            FileWriter fout = new FileWriter("scrittore.txt");
+            BufferedWriter bout = new BufferedWriter(fout);
+            //int i = 0;
+
+            while(true) {
+                s = b.readLine();
+                if(s==null)
+                    break;
+                System.out.println(s);
+                parole.add(s);
+            }
+            System.out.println(parole);
+            Collections.sort(parole);
+            System.out.println(parole);
+
+            while (true){
+
+            }
+
+        }catch(Exception e){
+
+        }
     }
 }
